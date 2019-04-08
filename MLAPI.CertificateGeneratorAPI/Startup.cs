f7ff6319e90fbc24940e73fd6b750a6978b13166
Albin Corén <twoten@twoten.io>
2019-04-08 23:30:16 +0200

    Added 404 page to API

--- MLAPI.CertificateGeneratorAPI/Startup.cs
+++ MLAPI.CertificateGeneratorAPI/Startup.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Security.Cryptography;
-using System.Threading;
 using System.Threading.Tasks;
 using Certes;
 using FluffySpoon.AspNet.LetsEncrypt;
@@ -12,7 +8,6 @@ using Markdig;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Rewrite.Internal.UrlMatches;
 using Microsoft.Extensions.DependencyInjection;
 using MLAPI.CertificateGeneratorCommon;
 using Octokit;
@@ -64,78 +59,86 @@ namespace MLAPI.CertificateGeneratorAPI
             app.UseFluffySpoonLetsEncryptChallengeApprovalMiddleware();
 
             app.Run(async (context) =>
-            {                
-                await Task.Factory.StartNew(() =>
+            {
+                if (context.Request.Method != "GET" || (context.Request.Path.HasValue && context.Request.Path.Value != "/"))
                 {
-                    GenerationLog log = new GenerationLog();
-                    log.Entries.Add(new LogEntry("Serving " + context.Connection.RemoteIpAddress));
-                    
-                    DateTime startTime = DateTime.UtcNow;
-                    DateTime endTime = DateTime.UtcNow.AddDays(30);
-
-                    log.Entries.Add(new LogEntry("Generating issuer key pair..."));
-                    RSAParameters issuerKeyPair = KeyGenerator.Get();
-                
-                    log.Entries.Add(new LogEntry("Generating certificate key pair..."));
-                    RSAParameters certificateKeyPair = KeyGenerator.Get();
-
-                    log.Entries.Add(new LogEntry("Generating certificate serial number..."));
-                    byte[] serialNumber = new byte[20];
-                    using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                    context.Response.StatusCode = 404;
+                    await context.Response.WriteAsync("Fuck off cunt Id=" + context.Connection.Id + " Address=" + context.Connection.RemoteIpAddress);
+                }
+                else
+                {
+                    await Task.Factory.StartNew(() =>
                     {
-                        // Should ensure non negativity and make it smaller
-                        // See https://tools.ietf.org/html/rfc3280#section-4.1.2.2
-                        rng.GetBytes(serialNumber);
-                    }
+                        GenerationLog log = new GenerationLog();
+                        log.Entries.Add(new LogEntry("Serving " + context.Connection.RemoteIpAddress));
 
-                    string issuerName = context.Request.Query.ContainsKey("issuer") ? context.Request.Query["issuer"].ToString() : "Unnamed Issuer";
-                    string certificateName = context.Request.Query.ContainsKey("name") ? context.Request.Query["name"].ToString() : "Unnamed MLAPI Development Certificate";
-                
-                    log.Entries.Add(new LogEntry("Generating certificate serial number..."));
-                    CertificateEmpire empire = Generator.GenerateCertificateEmpire(issuerKeyPair, certificateKeyPair, issuerName, certificateName, startTime, endTime, serialNumber, log);
+                        DateTime startTime = DateTime.UtcNow;
+                        DateTime endTime = DateTime.UtcNow.AddDays(30);
 
-                    Gist gist = null;
-                    
-                    if (Program.GITHUB_GIST_TOKEN != null)
-                    {
-                        log.Entries.Add(new LogEntry("Uploading gist..."));
-                        GitHubClient github = new GitHubClient(new ProductHeaderValue("MLAPI.Certificate.Generator"));
-                        github.Credentials = new Credentials(Program.GITHUB_GIST_TOKEN);
-                        
-                        gist = github.Gist.Create(new NewGist()
+                        log.Entries.Add(new LogEntry("Generating issuer key pair..."));
+                        RSAParameters issuerKeyPair = KeyGenerator.Get();
+
+                        log.Entries.Add(new LogEntry("Generating certificate key pair..."));
+                        RSAParameters certificateKeyPair = KeyGenerator.Get();
+
+                        log.Entries.Add(new LogEntry("Generating certificate serial number..."));
+                        byte[] serialNumber = new byte[20];
+                        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
                         {
-                            Description = "http://cert.midlevel.io/ Generated on " + DateTime.UtcNow + " by " + context.Connection.RemoteIpAddress,
-                            Files =
+                            // Should ensure non negativity and make it smaller
+                            // See https://tools.ietf.org/html/rfc3280#section-4.1.2.2
+                            rng.GetBytes(serialNumber);
+                        }
+
+                        string issuerName = context.Request.Query.ContainsKey("issuer") ? context.Request.Query["issuer"].ToString() : "Unnamed Issuer";
+                        string certificateName = context.Request.Query.ContainsKey("name") ? context.Request.Query["name"].ToString() : "Unnamed MLAPI Development Certificate";
+
+                        log.Entries.Add(new LogEntry("Generating certificate serial number..."));
+                        CertificateEmpire empire = Generator.GenerateCertificateEmpire(issuerKeyPair, certificateKeyPair, issuerName, certificateName, startTime, endTime, serialNumber, log);
+
+                        Gist gist = null;
+
+                        if (Program.GITHUB_GIST_TOKEN != null)
+                        {
+                            log.Entries.Add(new LogEntry("Uploading gist..."));
+                            GitHubClient github = new GitHubClient(new ProductHeaderValue("MLAPI.Certificate.Generator"));
+                            github.Credentials = new Credentials(Program.GITHUB_GIST_TOKEN);
+
+                            gist = github.Gist.Create(new NewGist()
                             {
+                                Description = "http://cert.midlevel.io/ Generated on " + DateTime.UtcNow + " by " + context.Connection.RemoteIpAddress,
+                                Files =
                                 {
-                                    "quickstart.md",
-                                    Generator.GetMarkdownInstructions(empire)
-                                }
-                            },
-                            Public = false
-                        }).Result;
+                                    {
+                                        "quickstart.md",
+                                        Generator.GetMarkdownInstructions(empire)
+                                    }
+                                },
+                                Public = false
+                            }).Result;
 
-                        log.EndTime = DateTime.UtcNow;
-                        
-                        if (Program.GIST_REDIRECT)
-                        {
-                            context.Response.Redirect(gist.HtmlUrl);
+                            log.EndTime = DateTime.UtcNow;
+
+                            if (Program.GIST_REDIRECT)
+                            {
+                                context.Response.Redirect(gist.HtmlUrl);
+                            }
                         }
-                    }
-                    
-                    if (!Program.GIST_REDIRECT)
-                    {
-                        log.EndTime = DateTime.UtcNow;
-                        
-                        using (StreamWriter writer = new StreamWriter(context.Response.Body))
+
+                        if (!Program.GIST_REDIRECT)
                         {
-                            MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
-                            
-                            writer.Write(Markdown.ToHtml(Generator.GetMarkdownInstructions(empire, gist == null ? null : gist.HtmlUrl), pipeline));
-                            writer.Write(Markdown.ToHtml(log.Serialize()), pipeline);
+                            log.EndTime = DateTime.UtcNow;
+
+                            using (StreamWriter writer = new StreamWriter(context.Response.Body))
+                            {
+                                MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UsePipeTables().Build();
+
+                                writer.Write(Markdown.ToHtml(Generator.GetMarkdownInstructions(empire, gist == null ? null : gist.HtmlUrl), pipeline));
+                                writer.Write(Markdown.ToHtml(log.Serialize()), pipeline);
+                            }
                         }
-                    }
-                });
+                    });
+                }
             });
         }
     }
